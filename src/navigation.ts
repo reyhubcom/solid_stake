71b5a044b1a414dbf0b7f69edee170734173fc3c
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-01-18 10:27:07 -0500

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -174,9 +174,9 @@ export const footerData = {
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="#"> Rey</a> · 2026 SolidStake.xyz All Rights Reserved.
   `,
 };
